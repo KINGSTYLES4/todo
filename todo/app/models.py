5f583d23783aa784d85c0a60c12c65280abcda34
unknown <praveen99887760@gmail.com>
2025-03-11 23:35:53 +0530

    registration creation

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=10)
+    address = models.CharField(max_length=50)
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-
+from app.forms import *
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def register(request):
-    return render(request,'register.html')
+    EUFO = UserForm()
+    EPFO = ProfileForm()
+    d = {'EUFO':EUFO , 'EPFO':EPFO}
+    if request.method == 'POST':
+        UFDO = UserForm(request.POST)
+        PFDO = ProfileForm(request.POST)
+        if UFDO.is_valid() and PFDO.is_valid():
+            pw = UFDO.cleaned_data.get('password')
+            MUFDO = UFDO.save(commit=False)
+            MUFDO.set_password(pw)
+            MPFDO = PFDO.save(commit=False)
+            MPFDO.username = MUFDO
+            MPFDO.save()
+            return HttpResponseRedirect(reverse('login'))
+        return HttpResponse('Invalid Data')
+    return render(request,'register.html',d)
 
 def login(request):
     return render(request,'login.html')
